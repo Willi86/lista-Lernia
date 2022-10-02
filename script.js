4496f3869a3cc346128b14d10a2e9af0dafb4ef2
Willi86 <williamlundqvist@yahoo.com>
2022-10-02 23:40:15 +0200

    First

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const input = document.querySelector("input");
+
+const addBtn = document.querySelector("button");
+
+const list = document.querySelector("ul");
+
+const toDoArray = [];
+
+
+
+addBtn.addEventListener("click", function() {
+
+let text = input.value;
+toDoArray.push(text);
+
+ //console.log(text);
+const item = document.createElement("li");
+list.appendChild(item);
+
+
+const itemLabel= document.createElement("span");
+itemLabel.innerText = text;
+
+item.appendChild(itemLabel);
+
+
+itemLabel.addEventListener("click", function() {
+
+if (item.getAttribute("class") == "completed") {
+    item.setAttribute("class", "");
+}
+
+
+else {
+     item.setAttribute("class", "completed");
+
+}
+
+}
+);
+
+}
+);
+
+
+
+
