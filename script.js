c439c7acec289bf1d28a8efafccc822a15641632
Willi86 <williamlundqvist@yahoo.com>
2022-10-03 02:27:38 +0200

    ...

--- script.js
+++ script.js
@@ -3,39 +3,65 @@ const input = document.querySelector("input");
 
 const addBtn = document.querySelector("button");
 
-const list = document.querySelector("ul");
+const list = document.querySelector("ul"); // ul is unordered list
 
-const toDoArray = [];
+const toDoArray = [];//array to store the todos
 
 
 
-addBtn.addEventListener("click", function() {
+addBtn.addEventListener("click", function() { // addBtn is clicked
 
 let text = input.value;
 toDoArray.push(text);
 
+if (!text){alert("Please enter a task!"); //if the user doesn't enter a task, an alert will pop up
+return;
+}
+else if (text.length > 10) {alert("Please enter a task less than 10 characters!");return;} //if the user enters a task longer than 10 characters, an alert will pop up
+else if (text.length < 50) {alert("Task added!");}
+
+//......................
+
+
+
+
+
  //console.log(text);
-const item = document.createElement("li");
-list.appendChild(item);
+const item = document.createElement("li"); //create a new list item
+list.appendChild(item); //add the new list item to the list
 
 
-const itemLabel= document.createElement("span");
-itemLabel.innerText = text;
+const itemLabel= document.createElement("span");  // create a span element
+itemLabel.innerText = text; //text is the input value
 
 item.appendChild(itemLabel);
 
+ input.value = "";  //clears input field  finally found it after 2 hours of searching
+const itemBtn = document.createElement("button"); //create button
+itemBtn.innerText= "Delete"; //add text to button
+item.appendChild(itemBtn); //add button to li
 
-itemLabel.addEventListener("click", function() {
+itemBtn.addEventListener("click", function() { //add event listener to button
+item.remove();
+});
 
-if (item.getAttribute("class") == "completed") {
-    item.setAttribute("class", "");
+
+
+
+
+
+itemLabel.addEventListener("click", function() { //add event listener to button
+
+if (item.getAttribute("class") == "completed") { //if class is completed
+    item.setAttribute("class", ""); //remove class
 }
 
 
 else {
-     item.setAttribute("class", "completed");
+     item.setAttribute("class", "completed"); //add class
 
-}
+} 
+
 
 
 
