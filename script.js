a685a3e92d51c9e6f0ffa559103353811d573ee4
Willi86 <williamlundqvist@yahoo.com>
2022-10-11 22:09:44 +0200

    styling

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const input = document.querySelector("input");
 
 const addBtn = document.querySelector("button");
@@ -7,67 +6,59 @@ const list = document.querySelector("ul"); // ul is unordered list
 
 const showCompleted = document.querySelector("p");
 
-const toDoArray = [];//array to store the todos
+const toDoArray = []; //array to store the todos
 
 let completedCount = 0;
 
-addBtn.addEventListener("click", function() { // addBtn is clicked
-let text = input.value;
-toDoArray.push(text);
- // toDo is the paragraph tag
+addBtn.addEventListener("click", function () {
+  // addBtn is clicked
+  let text = input.value;
+  toDoArray.push(text);
+  // toDo is the paragraph tag
 
-if (!text){alert("Please enter a task!"); //if the user doesn't enter a task, an alert will pop up
-return;
-}
-else if (text.length > 10) {alert("Please enter a task less than 10 characters!");return;} //if the user enters a task longer than 10 characters, an alert will pop up
-else if (text.length < 10) {alert("Task added!"); 
-}
-//forEach(item => { completedP ++;}); //completedP is the number of completed tasks
+  if (!text) {
+    alert("Please enter a task!"); //if the user doesn't enter a task, an alert will pop up
+    return;
+  } else if (text.length > 20) {
+    alert("Please enter a task less than 10 characters!");
+    return;
+  } //if the user enters a task longer than 10 characters, an alert will pop up
+  else if (text.length < 10) {
+    alert("Task added!");
+  }
+  //forEach(item => { completedP ++;}); //completedP is the number of completed tasks
 
-//......................
- //console.log(text);
-const item = document.createElement("li"); //create a new list item
-list.appendChild(item); //add the new list item to the list
+  //......................
+  //console.log(text);
+  const item = document.createElement("li"); //create a new list item
+  list.appendChild(item); //add the new list item to the list
 
+  const itemLabel = document.createElement("span"); // create a span element
+  itemLabel.innerText = text; //text is the input value
 
-const itemLabel= document.createElement("span");  // create a span element
-itemLabel.innerText = text; //text is the input value
+  item.appendChild(itemLabel);
 
-item.appendChild(itemLabel);
+  input.value = ""; //clears input field  finally found it after 2 hours of searching
+  const itemBtn = document.createElement("button"); //create button
+  itemBtn.innerText = "Delete"; //add text to button
+  item.appendChild(itemBtn); //add button to li
+  itemBtn.className = "BtnClass"; //add class to button
+  itemBtn.addEventListener("click", function () {
+    //add event listener to button
+    item.remove();
+  });
 
- input.value = "";  //clears input field  finally found it after 2 hours of searching
-const itemBtn = document.createElement("button"); //create button
-itemBtn.innerText= "Delete"; //add text to button
-item.appendChild(itemBtn); //add button to li
+  itemLabel.addEventListener("click", function () {
+    //add event listener to button
 
-itemBtn.addEventListener("click", function() { //add event listener to button
-item.remove();
+    if (item.getAttribute("class") == "completed") {
+      //if class is completed
+
+      item.setAttribute("class", "Not Completed"); //remove class
+    } else {
+      item.setAttribute("class", "completed"); //add class
+      completedCount++;
+      showCompleted.innerText = completedCount + " done"; //update completed count
+    }
+  });
 });
-
-itemLabel.addEventListener("click", function() { //add event listener to button
-
-if (item.getAttribute("class") == "completed") { //if class is completed
-
-    item.setAttribute("class", "Not Completed"); //remove class
- 
-}
-
-
-else {
-     item.setAttribute("class", "completed"); //add class
-     completedCount++;
-     showCompleted.innerText = completedCount +" done"; //update completed count
-
-} 
-
-
-}
-);
-
-
-}
-);
-
-
-
-
