ce4e1ec88139862e2a4ab97da6100733064d1e73
Willi86 <williamlundqvist@yahoo.com>
2022-10-22 17:44:12 +0200

    New styling, new things

--- script.js
+++ script.js
@@ -17,19 +17,45 @@ addBtn.addEventListener("click", function () {
   // toDo is the paragraph tag
 
   if (!text) {
-    alert("Please enter a task!"); //if the user doesn't enter a task, an alert will pop up
+    /* swal("Come on man", "Please enter a task!"); //if the user doesn't enter a task, an alert will pop up
+
+    const Alret = document.createElement("text");
+    Alret = "No empty text allowed";
+    document.body.appendChild(text); */
+    var divClass =
+      document.querySelector(
+        "#div1"
+      ); /*connecting the divClass with the id div1*/
+    divClass.style.display = "block"; /*Finally */ /*displaying the divClass*/
+    const myTimeout = setTimeout(
+      myGreeting,
+      2000
+    ); /*setting the timeout for 1 second,then next code will start */
+    function myGreeting() {
+      document.getElementById("div1").innerHTML = "";
+    } /*this function will change divClass be empty*/
     return;
-  } else if (text.length > 50) {
-    alert("Please enter a task less than 10 characters!");
+  } else if (text.length > 20) {
+    /*i removed the sweetalret and add the same as above so it will show the same message 
+  if i still have time i will search on how to have a new message instead, 
+  I still have a problem with making the code and the function run again, it only run once */
+    var divClass = document.querySelector("#div1");
+    divClass.style.display = "block"; /*Finally */
+    const myTimeout = setTimeout(myGreeting, 1000);
+    function myGreeting() {
+      document.getElementById("div1").innerHTML = "";
+    }
+
+    /*swal("Please enter a task less than 50 characters!");*/
     return;
-  } //if the user enters a task longer than 10 characters, an alert will pop up
-  else if (text.length < 50) {
-    alert("Task added!");
-  }
+  } /*if the user enters a task with more than 20 characters, an alert will pop up, But now i commented it
+   because its not needed anymore*/
+
+  /*else if (text.length < 20) {
+    swal("Task added!");
+  }*/
   //forEach(item => { completedP ++;}); //completedP is the number of completed tasks
 
-  //......................
-  //console.log(text);
   const item = document.createElement("li"); //create a new list item
   list.appendChild(item); //add the new list item to the list
 
